7df123a92dae5186e29f960ac217e8dfaa7cbafb
Alan Alvizo <aalvizo24@hotmail.com>
2024-10-17 16:14:08 -0600

    cambios en pool

--- src/routes/db.js
+++ src/routes/db.js
@@ -1,17 +1,31 @@
-require('dotenv').config()
-const mysql = require('mysql')
+require('dotenv').config();
+const mysql = require('mysql');
 
-// Crear conexión a la base de datos
-const connection = mysql.createConnection({
+// Crear pool de conexiones a la base de datos
+const pool = mysql.createPool({
+    connectionLimit: 10, // Ajusta según sea necesario
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+    database: process.env.DB_NAME,
+});
 
-connection.connect(err => {
-    if (err) throw err
-    console.log('Conectado a la base de datos')
-})
+// Manejar la conexión al pool
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('Error al obtener conexión del pool: ', err);
+        return;
+    }
+    console.log('Conectado a la base de datos');
 
-module.exports = connection
+    // Libera la conexión al pool después de su uso
+    connection.release();
+
+    // Manejar errores en el pool
+    pool.on('error', (err) => {
+        console.error('Error en el pool de conexiones: ', err);
+    });
+});
+
+// Exportar el pool para su uso en otras partes de la aplicación
+module.exports = pool;
