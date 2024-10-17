f17e75faa689a64df10117a3f972dfdd6726abbf
Alan Alvizo <aalvizo24@hotmail.com>
2024-10-17 16:16:12 -0600

    cambio en base de datos

--- src/app.js
+++ src/app.js
@@ -70,20 +70,7 @@ app.use('/', refacciones)
 app.use('/', mantenimiento)
 app.use('/', marcas)
 
-// Conexión a la base de datos
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
 
-connection.connect(err => {
-    if (err) throw err
-    console.log('Conectado a la base de datos de mrc')
-})
-
-module.exports = connection
 
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000
