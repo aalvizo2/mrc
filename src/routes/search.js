871140be30364f7a00f741158dda10444f57795d
Alan Alvizo <aalvizo24@hotmail.com>
2024-11-08 17:53:34 -0600

    cambios en buscar

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,13 +1,14 @@
 const express= require('express')
 const Router= express.Router()
 const connection= require('./db')
-const login= require('./login')
+const getImageUrl= require('./getImageUrl')
 
 Router.get('/search', (req, res)=>{
     const usuario= req.session.usuario
     connection.query('SELECT SUM (cantidad) as objeto FROM carrito WHERE usuario=?', [usuario], (err, objeto)=>{
         
         const contador= objeto[0].objeto
+        
         res.render('search', {
             login: true,
             usuario: usuario, 
@@ -21,17 +22,23 @@ Router.get('/buscar', (req, res)=>{
 Router.post('/buscar', (req,res)=>{
     const {buscar}= req.body
     connection.query('SELECT * FROM producto WHERE nombre_prod LIKE ?', ['%' + buscar + '%'], (err, busqueda)=>{
-        console.log(busqueda)
+        const imagenes= busqueda.map(imagen => imagen.img_product)
         const usuario= req.session.usuario
+
     connection.query('SELECT SUM (cantidad) as objeto FROM carrito WHERE usuario=?', [usuario], (err, objeto)=>{
         
         const contador= objeto[0].objeto
-        res.render('buscar', {
-            login: true,
-            usuario: usuario, 
-            objeto: contador, 
-            busqueda: busqueda
+        Promise.all(imagenes.map(imagen=> getImageUrl(imagen)))
+        .then((urls) =>{
+            res.render('buscar', {
+                login: true,
+                usuario: usuario, 
+                objeto: contador, 
+                busqueda: busqueda,
+                urls
+            })
         })
+        
     }) 
     })
 })
