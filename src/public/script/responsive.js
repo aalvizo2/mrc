2850d7570026b2f411f789aa3db023c5c56512c6
Alan Alvizo <aalvizo24@hotmail.com>
2024-11-28 14:51:41 -0600

    cambios en navbar y footer

--- src/public/script/responsive.js
+++ src/public/script/responsive.js
@@ -4,3 +4,14 @@ const menu = document.querySelector('header ul');
 menuToggle.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu_toggle');
+    const centerLinks = document.querySelector('.center-links');
+    const icons = document.querySelector('.icons');
+
+    menuToggle.addEventListener('click', () => {
+        centerLinks.classList.toggle('active');
+        icons.classList.toggle('active');
+    });
+});
