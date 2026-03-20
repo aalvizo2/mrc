926305519d60f92df612eef0b3cad9a1f51e0123
Alan Alvizo <aalvizo24@hotmail.com>
2026-03-19 19:54:11 -0600

    bug de la base de datos arreglado

--- src/routes/db.js
+++ src/routes/db.js
@@ -1,31 +1,31 @@
 require('dotenv').config();
 const mysql = require('mysql');
 
-// Crear pool de conexiones a la base de datos
 const pool = mysql.createPool({
-    connectionLimit: 10, // Ajusta según sea necesario
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+  connectionLimit: 10,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  port: process.env.DB_PORT || 3306,
+  password: process.env.DB_PASSWORD || '123456789',
+  database: process.env.DB_NAME || 'mrc',
 });
 
-// Manejar la conexión al pool
-pool.getConnection((err, connection) => {
+function connectDb() {
+  pool.getConnection((err, connection) => {
     if (err) {
-        console.error('Error al obtener conexión del pool: ', err);
-        return;
+      console.log('Error al conectar la base de datos')
+      setTimeout(connectDb, 2000)
+      return
     }
-    console.log('Conectado a la base de datos');
 
-    // Libera la conexión al pool después de su uso
-    connection.release();
+    console.log('Conectado correctamente a la base de datos')
+    connection.release()
+  })
 
-    // Manejar errores en el pool
-    pool.on('error', (err) => {
-        console.error('Error en el pool de conexiones: ', err)
-    });
-})
+}
 
-// Exportar el pool para su uso en otras partes de la aplicación
-module.exports = pool
+connectDb()
+
+
+
+module.exports = pool;
