4aefdcddc14990b3e4795952443b3c4f19ae4eb7
Alan Alvizo <aalvizo24@hotmail.com>
2026-04-28 20:29:20 -0600

    cambios en carousel

--- src/routes/getCarouselImageUrl.js
+++ src/routes/getCarouselImageUrl.js
@@ -7,13 +7,6 @@ const urlCache= {}
 
 function getCarouselUrl(imagen) {
     return new Promise((resolve, reject) => {
-        const now = Date.now()
-        const cachedUrl = urlCache[imagen]
-
-        if (cachedUrl && cachedUrl.expiresAt > now) {
-            return resolve(cachedUrl.url)
-        }
-
         minioClient.presignedUrl(
             'GET',
             'carousel',
@@ -21,12 +14,6 @@ function getCarouselUrl(imagen) {
             60,
             (err, url) => {
                 if (err) return reject(err)
-
-                urlCache[imagen] = {
-                    url,
-                    expiresAt: now + (1000 * 60 * 60 * 24)
-                }
-
                 resolve(url)
             }
         )
